dbb090850601d9c522bc6055e1de5b835998b92c
RaulilloSierra <raulitosierradiaz@gmail.com>
2024-11-13 14:35:01 -0500

    Formulario de presupuesto

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,7 +1,38 @@
-const BudgetForm = () => {
-  return (
-    <div>BudgetForm</div>
-  )
-}
+import { useState, ChangeEvent } from "react";
 
-export default BudgetForm
+const BudgetForm = () => {
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>)=>{
+    const { value } = e.target
+    setBudget(+value)
+  }
+  return (
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-sky-600 font-bold text-center"
+        >
+          Definir presupuesto
+        </label>
+        <input
+          id="budget"
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="Define tu presupuesto"
+          name="budget"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        value={"Definir presupuesto"}
+        className="bg-sky-600 hover:bg-sky-400 cursor-pointer w-full p-2 text-white font-black uppercase"
+      />
+    </form>
+  );
+};
+
+export default BudgetForm;
