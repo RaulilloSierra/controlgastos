3e3038186df97e81a34c843dfdef5c760343002b
RaulilloSierra <raulitosierradiaz@gmail.com>
2024-11-15 11:17:00 -0500

    Tipado datos gasto

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import DatePicker from "react-date-picker";
-import { categories } from "../data/categories";
+import { categories } from "../data/categories.ts";
 import "react-date-picker/dist/DatePicker.css";
 import "react-calendar/dist/Calendar.css";
-
-type ValuePiece = Date | null;
-
-type Value = ValuePiece | [ValuePiece, ValuePiece];
+import type { DraftExpense } from "../types";
 
 const ExpenseForm = () => {
+  const [expense, setExpense] = useState<DraftExpense>({
+    amount: 0,
+    expenseName: "",
+    category: "",
+    date: new Date(),
+  });
   return (
     <form className="space-y-5">
       <legend className="uppecase text-center text-2xl font-black border-b-4 border-sky-500 py-2">
@@ -23,6 +27,7 @@ const ExpenseForm = () => {
           placeholder="Añade el nombre del gasto"
           className="bg-slate-100 p-2 outline-none"
           name="expenseName"
+          value={expense.expenseName}
         />
       </div>
 
@@ -36,6 +41,7 @@ const ExpenseForm = () => {
           placeholder="Añade el valor del gasto"
           className="bg-slate-100 p-2 outline-none"
           name="amount"
+          value={expense.amount}
         />
       </div>
 
@@ -47,6 +53,7 @@ const ExpenseForm = () => {
           id="category"
           className="bg-slate-100 p-2 outline-none"
           name="category"
+          value={expense.category}
         >
           <option value="">-- Seleccione --</option>
           {categories.map((category) => (
@@ -61,7 +68,10 @@ const ExpenseForm = () => {
         <label htmlFor="date" className="text-xl">
           Fecha Gasto:
         </label>
-        <DatePicker className="bg-slate-100 p-2 border-0" />
+        <DatePicker
+          className="bg-slate-100 p-2 border-0"
+          value={expense.date}
+        />
       </div>
 
       <input
